bdbf0bc774605e65f8543d365c5f3fd26c36d8fd
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-03-01 12:10:50 +1030

    Add |server_addr| function to viper.ssl, like |raw_server_addr| from ngx.ssl but with formatting

--- ngx_http_viper_lua_ffi_ssl.c
+++ ngx_http_viper_lua_ffi_ssl.c
@@ -4,6 +4,12 @@
 
 #if NGX_HTTP_SSL
 
+enum {
+	NGX_HTTP_VIPER_ADDR_TYPE_UNIX  = 0,
+	NGX_HTTP_VIPER_ADDR_TYPE_INET  = 1,
+	NGX_HTTP_VIPER_ADDR_TYPE_INET6 = 2
+};
+
 int ngx_http_viper_lua_ffi_ssl_client_has_ecdsa(ngx_http_request_t *r, char **err)
 {
 	ngx_ssl_conn_t *ssl_conn;
@@ -59,6 +65,87 @@ int ngx_http_viper_lua_ffi_ssl_client_has_ecdsa(ngx_http_request_t *r, char **er
 #endif
 
 	return NGX_DECLINED;
+}
+
+int ngx_http_viper_lua_ffi_ssl_server_addr(ngx_http_request_t *r, char **addr, size_t *addrlen, int *addrtype, char **err)
+{
+	ngx_ssl_conn_t       *ssl_conn;
+	ngx_connection_t     *c;
+	struct sockaddr_in   *sin;
+#if (NGX_HAVE_INET6)
+	struct sockaddr_in6  *sin6;
+#endif
+#if (NGX_HAVE_UNIX_DOMAIN)
+	struct sockaddr_un   *saun;
+#endif
+
+	if (!r->connection || !r->connection->ssl) {
+		*err = "bad request";
+		return NGX_ERROR;
+	}
+
+	ssl_conn = r->connection->ssl->connection;
+	if (!ssl_conn) {
+		*err = "bad ssl conn";
+		return NGX_ERROR;
+	}
+
+	c = ngx_ssl_get_connection(ssl_conn);
+
+	if (ngx_connection_local_sockaddr(c, NULL, 0) != NGX_OK) {
+		return 0;
+	}
+
+	switch (c->local_sockaddr->sa_family) {
+#if (NGX_HAVE_INET6)
+		case AF_INET6:
+			sin6 = (struct sockaddr_in6 *)c->local_sockaddr;
+			*addr = ngx_palloc(r->pool, NGX_INET6_ADDRSTRLEN);
+			*addrlen = ngx_inet_ntop(AF_INET6, &sin6->sin6_addr.s6_addr, (u_char *)*addr, NGX_INET6_ADDRSTRLEN);
+
+			if (*addrlen == 0) {
+				ngx_pfree(r->pool, *addr); *addr = NULL;
+
+				*err = "ngx_inet_ntop failed";
+				return NGX_ERROR;
+			}
+
+			*addrtype = NGX_HTTP_VIPER_ADDR_TYPE_INET6;
+			break;
+#endif
+#if (NGX_HAVE_UNIX_DOMAIN)
+		case AF_UNIX:
+			saun = (struct sockaddr_un *)c->local_sockaddr;
+
+			/* on Linux sockaddr might not include sun_path at all */
+			if (c->local_socklen <= (socklen_t)offsetof(struct sockaddr_un, sun_path)) {
+				*addr = "";
+				*addrlen = 0;
+			} else {
+				*addr = saun->sun_path;
+				*addrlen = ngx_strlen(saun->sun_path);
+			}
+
+			*addrtype = NGX_HTTP_VIPER_ADDR_TYPE_UNIX;
+			break;
+#endif
+		default: /* AF_INET */
+			sin = (struct sockaddr_in *)c->local_sockaddr;
+			*addr = ngx_palloc(r->pool, NGX_INET_ADDRSTRLEN);
+			*addrlen = ngx_inet_ntop(AF_INET, &sin->sin_addr.s_addr, (u_char *)*addr, NGX_INET_ADDRSTRLEN);
+
+			if (*addrlen == 0) {
+				ngx_pfree(r->pool, *addr); *addr = NULL;
+
+				*err = "ngx_inet_ntop failed";
+				return NGX_ERROR;
+			}
+
+			*addrtype = NGX_HTTP_VIPER_ADDR_TYPE_INET;
+			break;
+	}
+
+	return NGX_OK;
 }
 
 #endif /* NGX_HTTP_SSL */
--- viper_ssl.lua
+++ viper_ssl.lua
@@ -6,16 +6,22 @@ local ffi_str = ffi.string
 local getfenv = getfenv
 local error = error
 local errmsg = base.get_errmsg_ptr()
+local get_size_ptr = base.get_size_ptr
 local FFI_DECLINED = base.FFI_DECLINED
 local FFI_OK = base.FFI_OK
 
 
 ffi.cdef[[
 int ngx_http_viper_lua_ffi_ssl_client_has_ecdsa(ngx_http_request_t *r, char **err);
+
+int ngx_http_viper_lua_ffi_ssl_server_addr(ngx_http_request_t *r, char **addr, size_t *addrlen, int *addrtype, char **err);
 ]]
 
 
 local _M = {}
+
+local charpp = ffi.new("char*[1]")
+local intp = ffi.new("int[1]")
 
 function _M.has_ecdsa()
 	local r = getfenv(0).__ngx_req
@@ -31,6 +37,33 @@ function _M.has_ecdsa()
 	end
 
 	return nil, ffi_str(errmsg[0])
+end
+
+local addr_types = {
+	[0] = "unix",
+	[1] = "inet",
+	[2] = "inet6",
+}
+
+function _M.server_addr()
+	local r = getfenv(0).__ngx_req
+	if not r then
+		return error("no request found")
+	end
+
+	local sizep = get_size_ptr()
+
+	local rc = C.ngx_http_viper_lua_ffi_ssl_server_addr(r, charpp, sizep, intp, errmsg)
+	if rc == FFI_OK then
+		local typ = addr_types[intp[0]]
+		if not typ then
+			return nil, nil, "unknown address type: " .. intp[0]
+		end
+
+		return ffi_str(charpp[0], sizep[0]), typ
+	end
+
+	return nil, nil, ffi_str(errmsg[0])
 end
 
 return _M
