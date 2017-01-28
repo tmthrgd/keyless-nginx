eeb0940a7bd6497f8d2c26a8e5833db94d7e7c44
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-01-29 10:26:57 +1030

    Do not test for unexpected tags in response

--- ngx_keyless_module.c
+++ ngx_keyless_module.c
@@ -925,14 +925,6 @@ static enum ssl_private_key_result_t ngx_http_keyless_operation_complete(ngx_htt
 				op->ocsp_response = CBS_data(&child);
 				op->ocsp_response_length = CBS_len(&child);
 				break;
-			case NGX_HTTP_KEYLESS_TAG_DIGEST:
-			case NGX_HTTP_KEYLESS_TAG_SNI:
-			case NGX_HTTP_KEYLESS_TAG_CLIENT_IP:
-			case NGX_HTTP_KEYLESS_TAG_SERVER_IP:
-			case NGX_HTTP_KEYLESS_TAG_SIG_ALGS:
-			case NGX_HTTP_KEYLESS_TAG_ECDSA_CIPHER:
-				ngx_log_error(NGX_LOG_ERR, op->log, 0, "keyless: unexpected tag");
-				return ssl_private_key_failure;
 		}
 	}
 
